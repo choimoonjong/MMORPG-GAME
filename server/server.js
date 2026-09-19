78c5a504b6762bb8834118e60e25a38b833dee53
choimoonjong <162957454+choimoonjong@users.noreply.github.com>
2026-09-19 15:47:58 +0900

    Prepare server for Railway deployment

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 const app = express();
 const server = http.createServer(app);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 require('dotenv').config();
 
